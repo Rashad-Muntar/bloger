540b4a2dad6579247ead373f13415db1a5464baa
Rashad-Muntar <muntartoure@gmail.com>
2021-04-25 21:40:25 +0000

    add article user association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
+    @article.user = User.first
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: "Article was successfully created." }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true, length: {minimum: 3, maxximum: 15}
     validates :email, presence: true, uniqueness: true
+    has_many :articles
 end
