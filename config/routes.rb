f1a5a6edcf470aa7d828fafa000165fe813377c9
Rashad-Muntar <muntartoure@gmail.com>
2021-04-25 23:06:25 +0000

    sign up user by ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
   resources :articles
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
