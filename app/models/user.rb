643884ee8fc52fc7b449955b9507ff29fbc91fd0
Rashad-Muntar <muntartoure@gmail.com>
2021-04-25 19:09:33 +0000

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: {minimum: 3, maxximum: 15}
+    validates :email, presence: true, uniqueness: true
 end
